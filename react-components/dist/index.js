651ae839232da25d88e2183170200901d76b24a2
Elliot <hack00mind@gmail.com>
2022-02-18 13:30:43 +0800

    chore: ignore dist

--- react-components/dist/index.js
+++
@@ -1,5 +0,0 @@
-import App from './App';
-import ReactDOM from 'react-dom';
-import React from 'react';
-var domContainer = document.querySelector('#react_component');
-ReactDOM.render(React.createElement(App), domContainer);
